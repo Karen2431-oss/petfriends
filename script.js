ff294a096940ed1e25471e85eb67604bf46b69b6
Karen Gil <gildaisy10@gmail.com>
2025-07-13 10:10:50 -0500

    Reestructura completa de index.html, style.css y script.js

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
 let carrito = [];
-let total = 0;
-
 function agregarAlCarrito(producto, precio) {
   carrito.push({ producto, precio });
-  actualizarCarrito();
-}
-
-function actualizarCarrito() {
-  const listaCarrito = document.getElementById('lista-carrito');
-  const totalElem = document.getElementById('total');
-  listaCarrito.innerHTML = '';
-
-  total = carrito.reduce((sum, item) => sum + item.precio, 0);
-
-  carrito.forEach((item, index) => {
-    const li = document.createElement('li');
-    li.textContent = `${item.producto} - $${item.precio.toFixed(2)}`;
-    listaCarrito.appendChild(li);
-  });
-
-  totalElem.textContent = `Total: $${total.toFixed(2)}`;
+  alert(`${producto} agregado al carrito. Total: $${carrito.reduce((s, p) => s + p.precio, 0).toFixed(2)}`);
 }
